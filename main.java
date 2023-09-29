84011c3d7c12f2448271cbccfe98ed599d5d66f7
Dtongg03 <120627116+dtongg03@users.noreply.github.com>
2023-09-29 23:02:30 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ package dem4;
 import java.util.*;
 public class main {
 	public static void main(String[] args) {
-	/*	List <diem> mylist = new ArrayList<diem>();
+		List <diem> mylist = new ArrayList<diem>();
 		diem a = new diem(1,1);
 		mylist.add(a);
 		diem b = new diem(2,2);
@@ -10,7 +10,7 @@ public class main {
 		diem c = new diem(3,3);
 		mylist.add(c);
 		mylist.forEach(x->{x.output();});
-		*/
+		
 	    Map <String, Integer> map = new HashMap<String, Integer>();
 	    map.put("MyString1", 1);
 	    map.put("MyString1", 2);
